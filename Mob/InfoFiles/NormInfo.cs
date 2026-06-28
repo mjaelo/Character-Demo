916b882868b83d0e307082996bf627c1768a1899
Marcin Jasiński <marcin.a.jasinski@capgemini.com>
2026-06-28 15:42:21 +0200

    NormInfo Bugfix

--- Mob/InfoFiles/NormInfo.cs
+++ Mob/InfoFiles/NormInfo.cs
@@ -3,16 +3,22 @@ using Godot;
 
 namespace CharacterDemo.Mob.InfoFiles;
 
-/// Encodes one norm rule used during mob generation/adjustment.
+/// Encodes one norm rule used during mob generation/adjustment. Fields cant be readonly, as it would break MobConstants.LoadNorms logic.
+/// 
 /// Forbidden whether value is forbidden or mandatory
-/// Enforce    whether this norm is always applied (ignores variation_chance).
+/// Enforce   whether this norm is always applied (ignores variation_chance).
+/// MeshNames list of mesh names this norm applies to.
+/// Tags      possible tags for any mesh
+/// Files     possible file names for meshName
+/// Colors    possible colors per material index for meshName
+/// Shapes    possible shape values for meshName
 public class NormInfo
 {
-    public readonly bool Forbidden = false;
-    public readonly bool Enforce = false;
-    public readonly IReadOnlyList<string> MeshNames = [];
-    public readonly IReadOnlyList<string> Tags = []; // possible tags for any mesh
-    public readonly IReadOnlyList<string> Files = []; // possible file names for meshName
-    public readonly IReadOnlyDictionary<int, List<Color>> Colors = new Dictionary<int, List<Color>>(); // possible colors per material index for meshName
-    public readonly IReadOnlyDictionary<int, float[]> Shapes = new Dictionary<int, float[]>(); // possible shape values for meshName
+    public bool Forbidden = false;
+    public bool Enforce = false;
+    public IReadOnlyList<string> MeshNames = [];
+    public IReadOnlyList<string> Tags = [];
+    public IReadOnlyList<string> Files = [];
+    public IReadOnlyDictionary<int, List<Color>> Colors = new Dictionary<int, List<Color>>();
+    public IReadOnlyDictionary<int, float[]> Shapes = new Dictionary<int, float[]>();
 }
